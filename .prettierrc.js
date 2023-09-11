811523882a08d57104aac36d66432ec5adfd7051
Soporior <zeref13@live.com>
2023-09-11 13:53:56 +0800

    test

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+/*$$
+ $ @Author: lxf
+ $ @Date: 2023-09-11 13:53:39
+ $ @LastEditors: lxf
+ $ @LastEditTime: 2023-09-11 13:53:42
+ $ @FilePath: \three.js\.prettierrc.js
+ $*/
+module.exports = {
+    stylelintIntegration: true,
+    eslintIntegration: true,
+    printWidth: 80, //单行长度
+    tabWidth: 4, //缩进长度
+    useTabs: false, //使用空格代替tab缩进
+    semi: true, //句末使用分号
+    singleQuote: true, //使用单引号
+    endOfLine: 'auto'
+};
